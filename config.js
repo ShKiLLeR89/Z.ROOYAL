44cf48ca4b0ee8ec2625fdc3f972f9c347bbc168
ShKiLLeR89 <nzarena81@gmail.com>
2026-07-09 21:23:01 +0330

    config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// ===== فایل config.js - تنظیمات تلگرام =====
+// ⚠️ این فایل رو توی گیت‌هاب آپلود نکن!
+
+const TELEGRAM_CONFIG = {
+    BOT_TOKEN: '8844891470:AAEyjSOO17Ui-JXNr-MTohZ5uM8JXRJ_DIs',
+    CHAT_ID: '103251505'
+};
